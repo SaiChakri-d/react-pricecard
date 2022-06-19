031dd8f5d1a6f18023e05fd71adbdffb9102d7dd
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2022-06-20 00:12:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,143 @@
 import logo from './logo.svg';
 import './App.css';
+import PriceCard from './PriceCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
+  let priceCard = [
+    {
+      title: 'Free',
+      price: 0,
+      features: [
+        {
+        name: "Single User",
+        isEnabled: true,
+        },
+        {
+        name: "5GB Storage",
+        isEnabled: true,
+        },
+        {
+        name: "Unlimited Public Projects",
+        isEnabled: true,
+        },
+        {
+        name: "Community Access",
+        isEnabled: true,
+        },
+        {
+        name: "Unlimited Private Projects",
+        isEnabled: false,
+        },
+        {
+        name: "Dedicated Phone Support",
+        isEnabled: false,
+        },
+        {
+        name: "Free Subdomain",
+        isEnabled: false,
+        },
+        {
+        name: "Monthly Status Reports",
+        isEnabled: false,
+        },
+      ]
+    },
+    {
+      title: 'Plus',
+      price: 29,
+      features: [
+        {
+        name: "5 Users",
+        isEnabled: true,
+        },
+        {
+        name: "50GB Storage",
+        isEnabled: true,
+        },
+        {
+        name: "Unlimited Public Projects",
+        isEnabled: true,
+        },
+        {
+        name: "Community Access",
+        isEnabled: true,
+        },
+        {
+        name: "Unlimited Private Projects",
+        isEnabled: true,
+        },
+        {
+        name: "Dedicated Phone Support",
+        isEnabled: true,
+        },
+        {
+        name: "Free Subdomain",
+        isEnabled: true,
+        },
+        {
+        name: "Monthly Status Reports",
+        isEnabled: false,
+        },
+      ]
+    },
+    {
+      title: 'Pro',
+      price: 49,
+      features: [
+        {
+        name: "Unlimited Users",
+        isEnabled: true,
+        },
+        {
+        name: "150GB Storage",
+        isEnabled: true,
+        },
+        {
+        name: "Unlimited Public Projects",
+        isEnabled: true,
+        },
+        {
+        name: "Community Access",
+        isEnabled: true,
+        },
+        {
+        name: "Unlimited Private Projects",
+        isEnabled: true,
+        },
+        {
+        name: "Dedicated Phone Support",
+        isEnabled: true,
+        },
+        {
+        name: "Free Subdomain",
+        isEnabled: true,
+        },
+        {
+        name: "Monthly Status Reports",
+        isEnabled: true,
+        },
+      ]
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <section className="pricing py-5">
+       <div className="container">
+       <div className="row">
+         {
+          priceCard.map((card)=> {
+            return <PriceCard data={card}></PriceCard>
+          })
+         }
+       </div>
+       </div>
+    </section>
+    </>
   );
 }
 
--- src/PriceCard.js
+++ src/PriceCard.js
@@ -1,3 +1,39 @@
-function PriceCard(){
-    return
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons';
+
+function PriceCard({ data }){ //Object destructuring concept
+    return(
+        <>
+       <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">{data.title}</h5>
+            <h6 className="card-price text-center">${data.price}<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="fa-ul">
+            {
+              data.features.map((list)=>{
+                    return <li className={list.isEnabled ? '' : 'text-muted'}>
+                            <span>
+                            {list.isEnabled ? <FontAwesomeIcon icon={faCheck}/> : <FontAwesomeIcon icon={faTimes}/>}
+                            
+                            {list.name}
+                            </span>
+                        </li>
+                    
+              })
+            }
+            </ul>
+        <div className="d-grid">
+        <a href="#" className="btn btn-primary text-uppercase">Button</a>
+        </div>
+        </div>
+        </div>
+      </div>
+        
+      </>
+    );
 }
+
+export default PriceCard;
