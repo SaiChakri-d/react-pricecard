f96aff25fa226ea587b1464132b5103747f3b38d
Sai Chakri D <98957092+SaiChakri-d@users.noreply.github.com>
2022-06-20 12:36:37 +0530

    Updated the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import PriceCard from './PriceCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCheck, faTimes} from '@fortawesome/free-solid-svg-icons';
 
 
 function App() {
@@ -127,15 +125,13 @@ function App() {
   return (
     <>
     <section className="pricing py-5">
-       <div className="container">
+      <div className="container">
        <div className="row">
-         {
-          priceCard.map((card)=> {
+         {priceCard.map((card)=> {
             return <PriceCard data={card}></PriceCard>
-          })
-         }
+          })}
        </div>
-       </div>
+      </div>
     </section>
     </>
   );
--- src/PriceCard.js
+++ src/PriceCard.js
@@ -8,22 +8,25 @@ function PriceCard({ data }){ //Object destructuring concept
        <div className="col-lg-4">
         <div className="card mb-5 mb-lg-0">
           <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">{data.title}</h5>
-            <h6 className="card-price text-center">${data.price}<span className="period">/month</span></h6>
+            <h5 className="card-title text-muted text-uppercase text-center">
+              {data.title}
+            </h5>
+            <h6 className="card-price text-center">
+              ${data.price}
+              <span className="period">/month</span>
+            </h6>
             <hr/>
             <ul className="fa-ul">
-            {
-              data.features.map((list)=>{
-                    return <li className={list.isEnabled ? '' : 'text-muted'}>
+            {data.features.map((list)=>{
+                    return (
+                    <li className={list.isEnabled ? '' : 'text-muted'}>
                             <span>
                             {list.isEnabled ? <FontAwesomeIcon icon={faCheck}/> : <FontAwesomeIcon icon={faTimes}/>}
-                            
-                            {list.name}
+                            &nbsp;&nbsp;{list.name}
                             </span>
-                        </li>
-                    
-              })
-            }
+                    </li>  
+                   );
+              })}
             </ul>
         <div className="d-grid">
         <a href="#" className="btn btn-primary text-uppercase">Button</a>
